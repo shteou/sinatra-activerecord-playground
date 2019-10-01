2573946d36dde78af3bab2b4fb56b5f7b78e6ac0
Stewart Platt <shteou@gmail.com>
2019-10-01 21:16:41 +0100

    Simplify run script

--- run.sh
+++ run.sh
@@ -7,5 +7,4 @@ docker network create foo
 docker run --network 'foo' --name sinatra -d                                    -p 8080:8080 sinatra
 docker run --network 'foo' --name mysql   -d --env MYSQL_ALLOW_EMPTY_PASSWORD=1 -p 3306:3306 mysql:5.6
 sleep 10
-echo 'CREATE DATABASE test;' | mysql -u root -h 127.0.0.1 
-docker exec -it sinatra bundle exec rake db:migrate
+docker exec -it sinatra bundle exec rake db:create db:migrate
